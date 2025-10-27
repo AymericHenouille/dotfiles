cb32d3db00f3ee9f54b5f75c0fb3d781d3750496
Aymeric Hénouille <30652970+AymericHenouille@users.noreply.github.com>
2025-10-27 17:09:43 +0100

    fix: termguicolor error for neovim

--- neovim/lua/plugins/themes/rose-pine.lua
+++ neovim/lua/plugins/themes/rose-pine.lua
@@ -4,9 +4,9 @@ return {
   opts = {
   },
 	config = function(_, opts)
+    vim.api.nvim_set_option_value("termguicolors", true, {})
     local rosepine = require("rose-pine")
     rosepine.setup(opts)
-    vim.api.nvim_set_option_value("termguicolors", true, {})
 		vim.cmd("colorscheme rose-pine")
 	end
 }
